09b789f868c70741d789577bc16de88dd7aa68ff
hyunsap <marianazunino2003@gmail.com>
2024-03-31 19:06:46 -0300

    tres en linea

--- src/lib/IrregularPath.svelte
+++ src/lib/IrregularPath.svelte
@@ -5,6 +5,22 @@
   let numbers = [10, 25, 33, 62, 75, 100]
   let pathLength
   let largo
+  let circles = [ //para pintar los circulos a medida que aumenta el progreso sobre el recorrido
+    { id: 'p1', cx: 42, cy: 70, threshold: 10 },
+    { id: 'p2', cx: 62, cy: 98, threshold: 16 },
+    { id: 'p3', cx: 94, cy: 130, threshold: 24 },
+    { id: 'p4', cx: 134, cy: 166, threshold: 33 },
+    { id: 'p5', cx: 174, cy: 186, threshold: 42 },
+    { id: 'p6', cx: 194, cy: 210, threshold: 47 },
+    { id: 'p7', cx: 218, cy: 234, threshold: 53 },
+    { id: 'p8', cx: 238, cy: 258, threshold: 59 },
+    { id: 'p9', cx: 254, cy: 282, threshold: 64 },
+    { id: 'p10', cx: 274, cy: 306, threshold: 69 },
+    { id: 'p11', cx: 294, cy: 338, threshold: 80 },
+    { id: 'p12', cx: 354, cy: 354, threshold: 89 },
+    { id: 'p13', cx: 370, cy: 374, threshold: 93 },
+    { id: 'p14', cx: 398, cy: 398, threshold: 100 }
+  ];
 
 
   onMount(() => {
@@ -14,10 +30,6 @@
     // longitud total del path
     let maxGap = pathLength - (pathLength * d3.min(numbers) / d3.max(numbers))
     let minGap = 0
-
-    // console.log("total path", pathLength)
-    // console.log("maximo gap", maxGap)
-    // console.log("minimo gap", maxGap)
 
     largo = function (n) {
       let scale = d3
@@ -31,116 +43,61 @@
 
 <div class="linea">
 {#each numbers as n}
-    <div>
-      <svg width="600" height="600">
+    <div class="grafico">
+      <svg width="410" height="440">
         <!-- Linea D -->
         <polyline id="polyline"
-          points="52.25,60 90,125 115,160 155,200 205,245 255,270 280,300 310,330 335,360 355,390 
-          380,420 385,460 480,480 500,505 535,535"
+          points="10.8,28 42,70 62,98 94,130 134,166 174,186 194,210 218,234 238,258 254,282 274,306 294,338 354,354 370,374 398,398"
           fill="transparent"
           stroke="#ccc"
           stroke-width="5"
         />
         <g id="puntos">
-          <circle cx="52.25" cy="60" r="5" fill="white" stroke="black" />
-          <text x="52.25" y="40" text-anchor="middle" alignment-baseline="middle">Congreso</text>
-          <circle id="p1" cx="90" cy="125" r="5" fill="white" stroke="black"/>
-          <circle id="p2" cx="115" cy="160" r="5" fill="white" stroke="black"/>
-          <circle id="p3" cx="155" cy="200" r="5" fill="white" stroke="black" />
-          <circle id="p4" cx="205" cy="245" r="5" fill="white" stroke="black" />
-          <circle id="p5" cx="255" cy="270" r="5" fill="white" stroke="black" />
-          <circle id="p6" cx="280" cy="300" r="5" fill="white" stroke="black" />
-          <circle id="p7" cx="310" cy="330" r="5" fill="white" stroke="black" />
-          <circle id="p8" cx="335" cy="360" r="5" fill="white" stroke="black" />
-          <circle id="p9" cx="355" cy="390" r="5" fill="white" stroke="black" />
-          <circle id="p10" cx="380" cy="420" r="5" fill="white" stroke="black" />
-          <circle id="p11" cx="385" cy="460" r="5" fill="white" stroke="black" />
-          <circle id="p12" cx="480" cy="480" r="5" fill="white" stroke="black" />
-          <circle id="p13" cx="500" cy="505" r="5" fill="white" stroke="black" />
-          <circle id="p14" cx="535" cy="535" r="5" fill="white" stroke="black" />
-          <text x="535" y="555" text-anchor="middle" alignment-baseline="middle">Catedral</text>
+          <circle cx="10.8" cy="28" r="5" fill="white" stroke="black" />
+          <text x="35.45" y="8" text-anchor="middle" alignment-baseline="middle">Congreso</text>
+          <circle cx="42" cy="70" r="5" fill="white" stroke="black"/>
+          <circle cx="62" cy="98" r="5" fill="white" stroke="black"/>
+          <circle cx="94" cy="130" r="5" fill="white" stroke="black" />
+          <circle cx="134" cy="166" r="5" fill="white" stroke="black" />
+          <circle cx="174" cy="186" r="5" fill="white" stroke="black" />
+          <circle cx="194" cy="210" r="5" fill="white" stroke="black" />
+          <circle cx="218" cy="234" r="5" fill="white" stroke="black" />
+          <circle cx="238" cy="258" r="5" fill="white" stroke="black" />
+          <circle cx="254" cy="282" r="5" fill="white" stroke="black" />
+          <circle cx="274" cy="306" r="5" fill="white" stroke="black" />
+          <circle cx="294" cy="338" r="5" fill="white" stroke="black" />
+          <circle cx="354" cy="354" r="5" fill="white" stroke="black" />
+          <circle cx="370" cy="374" r="5" fill="white" stroke="black" />
+          <circle cx="398" cy="398" r="5" fill="white" stroke="black" />
+          <text x="378" y="420" text-anchor="middle" alignment-baseline="middle">Catedral</text>
         </g>
 
 
         <!-- Progreso -->
         {#if pathLength}
-          <polyline id="polyline" 
-              points="52.25,60 90,125 115,160 155,200 205,245 255,270 280,300 310,330 335,360 355,390 
-              380,420 385,460 480,480 500,505 535,535"
-              fill="none"
-              stroke="green"
-              stroke-width="5"
-              stroke-dasharray={pathLength}
-              stroke-dashoffset={largo(n)}
-          />
-          <g id="puntos">
-            <circle  cx="52.25" cy="60" r="5" fill="green" stroke="black"/>
-          </g>
-        {/if}
-        
-        
-        {#if n >= 10}
-          <circle id="p1" cx="90" cy="125" r="5" fill="green" stroke="black"/>
+        <polyline id="polyline" 
+            points="10.8,28 42,70 62,98 94,130 134,166 174,186 194,210 218,234 238,258 254,282 274,306 294,338 354,354 370,374 398,398"
+            fill="none"
+            stroke="green"
+            stroke-width="5"
+            stroke-dasharray={pathLength}
+            stroke-dashoffset={largo(n)}
+        />
+        <g id="primera estacion">
+          <circle  cx="10.8" cy="28" r="5" fill="green" stroke="green"/>
+        </g>
         {/if}
 
-        {#if n >= 16}
-          <circle id="p2" cx="115" cy="160" r="5" fill="green" stroke="black"/>
-        {/if}
-
-        {#if n >= 24}
-          <circle id="p3" cx="155" cy="200" r="5" fill="green" stroke="black" />
-        {/if}
-
-        {#if n >= 34}
-          <circle id="p4" cx="205" cy="245" r="5" fill="green" stroke="black" />
-        {/if}
-
-        {#if n >= 42}
-          <circle id="p5" cx="255" cy="270" r="5" fill="green" stroke="black" />
-        {/if}
-
-        {#if n >= 47}
-          <circle id="p6" cx="280" cy="300" r="5" fill="green" stroke="black" />
-        {/if}
-
-        {#if n >= 53}
-          <circle id="p7" cx="310" cy="330" r="5" fill="green" stroke="black" />
-        {/if}
-
-        {#if n >= 59}
-          <circle id="p8" cx="335" cy="360" r="5" fill="green" stroke="black" />
-        {/if}
-
-        {#if n >= 64}
-          <circle id="p9" cx="355" cy="390" r="5" fill="green" stroke="black" />
-        {/if}
-
-        {#if n >= 69}
-          <circle id="p10" cx="380" cy="420" r="5" fill="green" stroke="black" />
-        {/if}
-
-        {#if n >= 75}
-        <circle id="p11" cx="385" cy="460" r="5" fill="green" stroke="black" />
-        {/if}
-
-        {#if n >= 89}
-          <circle id="p12" cx="480" cy="480" r="5" fill="green" stroke="black" />
-        {/if}
-        
-        {#if n >= 93}
-          <circle id="p13" cx="500" cy="505" r="5" fill="green" stroke="black" />
-        {/if}
-        
-        {#if n == 100}
-          <circle id="p14" cx="535" cy="535" r="5" fill="green" stroke="black" />
-        {/if}
-        
+        {#each circles as circle}
+          {#if n >= circle.threshold}
+            <circle id={circle.id} cx={circle.cx} cy={circle.cy} r="5" fill="green" stroke="green"/>
+          {/if}
+        {/each}
+      
       </svg>
+      
       <p class="number">{n}% del viaje realizado</p>
 
-      <br>
-      <br>
-      <br>
     </div>
     {/each}
   </div>
@@ -148,12 +105,18 @@
 <style>
   .linea{
     display: flex;
-    /*flex-direction: column;*/
     flex-wrap: wrap;
     justify-content: center;
     align-items: space-between;
+    
+  }
+
+  .grafico {
+    margin-bottom: 100px;
   }
   .number {
     text-align: center;
+    font-size: 20px;
+    color: #00640E
   }
 </style>
